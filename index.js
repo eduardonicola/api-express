35627be7eebbc1c19d0ac273f09a00b0610f6b33
eduardo-possani <possaniduardo@gmail.com>
2024-03-05 22:07:13 -0300

    rotas

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+app.use(express.json())
 
 // Rota padrão
 app.get('/', (req, res) => {
     res.send('API EXPRESS');
 });
-//
+
 app.get('/:cor', (req, res) => {
     const cores = ['azul','amarerlo','vermelho','roxo']
     if(cores.includes(req.params.cor)){
@@ -18,6 +19,16 @@ app.get('/:cor', (req, res) => {
     
 });
 
+app.post('/cor', (req, res) => {
+    const cores = ['azul','amarerlo','vermelho','roxo']
+
+    if(cores.includes(req.body.cor)){
+        res.json({cor: req.body.cor });
+        return
+    }
+    res.status(400).json({error: `${req.body.cor} Não é uma cor valida` })
+    
+});
 // Iniciar o servidor
 app.listen(port, () => {
     console.log(`Servidor está rodando em http://localhost:${port}`);
